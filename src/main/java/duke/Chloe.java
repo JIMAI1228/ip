ab7c5779bef8446bfa42fdaa409e8bf9529ffcc9
吉麦 <jimai@jimaideMacBook-Pro.local>
2026-01-25 20:20:01 +0800

    Level-0

---
+++ src/main/java/duke/Chloe.java
@@ -0,0 +1,9 @@
+package duke;
+
+public class Chloe {
+    public void run(){
+        System.out.println("Hello! I'm Chloe");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
---
+++ src/main/java/duke/Main.java
@@ -0,0 +1,8 @@
+package duke;
+
+public class Main {
+    public static void main(String[] args){
+        Chloe chloe = new Chloe();
+        chloe.run();
+    }
+}
