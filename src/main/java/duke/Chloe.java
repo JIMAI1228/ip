18bbdd44d88ee5e0d0723e4f74d63ee2c11e17e3
吉麦 <jimai@jimaideMacBook-Pro.local>
2026-01-26 00:46:57 +0800

    Level-1

--- src/main/java/duke/Chloe.java
+++ src/main/java/duke/Chloe.java
@@ -1,9 +1,22 @@
 package duke;
 
+import java.util.Scanner;
+
 public class Chloe {
     public void run(){
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! I'm Chloe");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true){
+            String input = scanner.nextLine();
+
+            if(input.equals("bye")){
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(input);
+        }
     }
 }
