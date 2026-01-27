3d7eed5f88f6ecd531611a78ffd4884e39def6e5
吉麦 <jimai@192.168.1.71>
2026-01-27 16:13:05 +0800

    Level-5

--- src/main/java/duke/Chloe.java
+++ src/main/java/duke/Chloe.java
@@ -12,74 +12,99 @@ public class Chloe {
         System.out.println("Hello! I'm Chloe");
         System.out.println("What can I do for you?");
 
-        while (true){
-            String input = scanner.nextLine();
+        while (true) {
+            try {
+                String input = scanner.nextLine();
 
-            if(input.equals("bye")){
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            }
-
-            if(input.equals("list")){
-                for(int i = 0; i < taskCount; i++){
-                    System.out.println((i + 1) + ". " + tasks[i].toString());
+                if (input.equals("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
                 }
-                continue;
-            }
 
-            if(input.startsWith("mark ")){
-                int index = Integer.parseInt(input.substring(5)) - 1;
-                tasks[index].markAsDone();
-                Task t = tasks[index];
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println("  [X] " + t.getDescription());
-                continue;
-            }
+                if (input.equals("list")) {
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println("    " + (i + 1) + ". " + tasks[i].toString());
+                    }
+                    continue;
+                }
 
-            if(input.startsWith("unmark ")){
-                int index = Integer.parseInt(input.substring(7)) - 1;
-                tasks[index].markAsNotDone();
-                Task t = tasks[index];
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("  [ ] " + t.getDescription());
-                continue;
-            }
+                if(input.equals("mark")){
+                    throw new ChloeException("The description of a mask cannot be empty.");
+                }
 
+                if (input.startsWith("mark ")) {
+                    int index = Integer.parseInt(input.substring(5)) - 1;
+                    tasks[index].markAsDone();
+                    Task t = tasks[index];
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  [X] " + t.getDescription());
+                    continue;
+                }
 
-            if(input.startsWith("todo ")){
-                String desc = input.substring(5);
-                tasks[taskCount] = new ToDo(desc);
-                Task t = tasks[taskCount];
-                taskCount++;
-                System.out.println("Got it. I've added this task:");
-                System.out.println("  " + t.toString());
-                System.out.println("Now you have " + taskCount + " tasks in the list.");
-                continue;
-            }
-            if (input.startsWith("deadline ")) {
-                String[] parts = input.substring(9).split(" /by ");
-                tasks[taskCount] = new Deadline(parts[0], parts[1]);
-                Task t = tasks[taskCount];
-                taskCount++;
-                System.out.println("Got it. I've added this task:");
-                System.out.println("  " + t.toString());
-                System.out.println("Now you have " + taskCount + " tasks in the list.");
-                continue;
-            }
-            if(input.startsWith("event ")){
-                String[] parts = input.substring(6).split(" /from | /to ");
-                tasks[taskCount] = new Event(parts[0], parts[1], parts[2]);
-                Task t = tasks[taskCount];
-                taskCount++;
-                System.out.println("Got it. I've added this task:");
-                System.out.println("  " + t.toString());
-                System.out.println("Now you have " + taskCount + " tasks in the list.");
-                continue;
-            }
+                if(input.equals("unmark")){
+                    throw new ChloeException("The description of an unmask cannot be empty.");
+                }
 
-            System.out.println("added:" + input);
-            tasks[taskCount] = new Task(input);
-            taskCount++;
+                if (input.startsWith("unmark ")) {
+                    int index = Integer.parseInt(input.substring(7)) - 1;
+                    tasks[index].markAsNotDone();
+                    Task t = tasks[index];
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("  [ ] " + t.getDescription());
+                    continue;
+                }
+
+                if(input.equals("todo")){
+                    throw new ChloeException("The description of a todo cannot be empty.");
+                }
+
+                if (input.startsWith("todo ")) {
+                    String desc = input.substring(5);
+                    tasks[taskCount] = new ToDo(desc);
+                    Task t = tasks[taskCount];
+                    taskCount++;
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("  " + t.toString());
+                    System.out.println("Now you have " + taskCount + " tasks in the list.");
+                    continue;
+                }
+
+                if(input.equals("deadline")){
+                    throw new ChloeException("The description of a deadline cannot be empty.");
+                }
+
+                if (input.startsWith("deadline ")) {
+                    String[] parts = input.substring(9).split(" /by ");
+                    tasks[taskCount] = new Deadline(parts[0], parts[1]);
+                    Task t = tasks[taskCount];
+                    taskCount++;
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("  " + t.toString());
+                    System.out.println("Now you have " + taskCount + " tasks in the list.");
+                    continue;
+                }
+
+                if(input.equals("event")){
+                    throw new ChloeException("The description of an event cannot be empty.");
+                }
+
+                if (input.startsWith("event ")) {
+                    String[] parts = input.substring(6).split(" /from | /to ");
+                    tasks[taskCount] = new Event(parts[0], parts[1], parts[2]);
+                    Task t = tasks[taskCount];
+                    taskCount++;
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("  " + t.toString());
+                    System.out.println("Now you have " + taskCount + " tasks in the list.");
+                    continue;
+                }
+
+                throw new ChloeException("I'm sorry, I don't know what that means.");
+
+            } catch (ChloeException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 }
---
+++ src/main/java/duke/ChloeException.java
@@ -0,0 +1,7 @@
+package duke;
+
+public class ChloeException extends Exception{
+    public ChloeException(String message){
+        super(message);
+    }
+}
