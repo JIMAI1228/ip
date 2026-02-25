c868338d8cb3cef0faf177b2305ece3de5123f50
吉麦 <jimai@jimaideMacBook-Pro.local>
2026-02-25 17:19:41 +0800

    Polish GUI by cropping profile pictures into circles

--- src/main/java/duke/ui/DialogBox.java
+++ src/main/java/duke/ui/DialogBox.java
@@ -13,6 +13,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
 
 /**
  * Represents a dialog box consisting of an ImageView to represent the speaker's face
@@ -37,6 +38,11 @@ public class DialogBox extends HBox {
 
         dialog.setText(text);
         displayPicture.setImage(img);
+
+        double radius = Math.min(displayPicture.getFitWidth(), displayPicture.getFitHeight()) / 2.0;
+        Circle clip = new Circle(radius, radius, radius);
+        displayPicture.setClip(clip);
+
         dialog.getStyleClass().add("dialog-bubble");
     }
 
